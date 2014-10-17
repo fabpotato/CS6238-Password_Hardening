1578a002ef0da9e873534f3ac1f1051ca5ab5112
Harsh Chawla <hchawla@localhost.localdomain>
2014-10-17 00:34:38 -0400

    And now my back is Fucked up. But Hey Lagrange's Factor is working.
    Signed-off-by: Harsh Chawla <hchawla@localhost.localdomain>

---
+++ Project Folder/LoginAttempt.java
@@ -0,0 +1,160 @@
+package edu.gatech.scs.pwd.hardening;
+import java.math.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+public class LoginAttempt{
+
+	
+	BigInteger[] featureValuesReceived = new BigInteger[15];
+	private String fileDir = ""; 
+	public String pwd;
+	
+	
+	BigInteger[] thresholdValues = new BigInteger[15];
+	
+	public BigInteger[] loginAttempt() throws NumberFormatException, IOException{
+		
+		//Generating a string that denotes the path to the directory of loginfeaturevalues.txt file. 
+		fileDir = new String("loginAndThresholdFeatureValues"
+                + File.separator);
+		
+		
+		
+		//Using an external library to parse through the CSV file of login features that we pretend we received from the client.
+	
+			CSVReader reader = new CSVReader(new FileReader(fileDir + "loginfeaturevalues.txt"));
+			
+			
+			//To  temporarily read the lines from the file
+		    String [] nextLine;
+		    
+		    
+		    //Iterating through the lines
+		    while ((nextLine = reader.readNext()) != null) {
+		        // nextLine[] is an array of values from the line
+		        	    	
+		    	//Extracting password and featurevalues from the loginfeaturevalues.txt file and storing them in an integer array.
+		    	
+		    	pwd = nextLine[15]; //Password is the last value in the loginfeaturevalues csv file
+		    	System.out.println(pwd);
+		    	
+		    	for(int i=0; i<15; i++){
+		    
+		    			featureValuesReceived[i] = BigInteger.valueOf(Integer.parseInt(nextLine[i]));
+		    			System.out.println(featureValuesReceived[i]);
+		    	}
+		    }
+			
+		    reader.close();
+				
+		return featureValuesReceived;
+		
+		
+	}
+	
+	
+	
+	//For reading threshold values and storing in an array of integers
+	public BigInteger[] fetchThresholdValues() throws NumberFormatException, IOException{
+		
+		//Generating a string that denotes the path to the directory of thresholdvalues.txt file. 
+		fileDir = new String("loginAndThresholdFeatureValues"
+                + File.separator);
+		
+		
+			//Using an external library to parse through the CSV file of threshold values
+			CSVReader reader = new CSVReader(new FileReader(fileDir + "threshold.txt"));
+						
+			
+			//To  temporarily read the lines from the file
+		    String [] nextLine;
+		    
+		    //Iterating through all the lines in the file
+		    while ((nextLine = reader.readNext()) != null) {
+		        // nextLine[] is an array of values from the line
+		   		    	
+		    	
+		    	//Extracting thresholdvalues from the thresholdvalues.txt file and storing them in an integer array.
+		    	for(int i=0; i<15; i++){
+		    		
+		    			thresholdValues[i] = BigInteger.valueOf(Integer.parseInt(nextLine[i]));
+		    			System.out.println("\n"+thresholdValues[i]);
+		    	}
+		    }
+			
+			
+		    reader.close();
+		
+		return thresholdValues;
+	}
+	
+	
+	
+	
+	//Calculate mean of feature values by fetching them from the history file
+	public int calculateMean(int[] d){
+		
+		
+		return 0;
+		
+	}
+	
+	
+	//Calculate median of feature values by fetching them from the history file
+	public int calculateMedian(int[] d){
+		
+		return 0;
+	}
+	
+	
+	//Based on mean and standard deviation
+	public int selectShare(){
+		
+		
+		return 1;
+		
+	}
+	
+	
+	//To calculate the y co-ordinates
+	public BigInteger calculateHpwdnew(BigInteger[] y,BigInteger[] x){
+		
+		BigInteger hpwd=BigInteger.valueOf(0);
+		for(int i =0;i<15;i++)
+		{
+			hpwd=hpwd.add(y[i].multiply(calculateLagrangeMultiplier(x[i],i,x)));
+		}
+		return hpwd;
+	}
+	
+	
+	//Calculating lagrange multiplier
+	public BigInteger calculateLagrangeMultiplier(BigInteger xi,int i, BigInteger[] x){
+		
+		BigInteger LagrangeMultiplier=BigInteger.valueOf(1);
+		for(int j=0;j<15;j++)
+		{	
+			if(j==i)
+			{
+				//Do nothing
+				System.out.println("Catching j=i");
+			}
+			else
+			{
+			System.out.println("Value of x[j] and x[i] is "+x[j]+" and "+x[i]);
+			LagrangeMultiplier=LagrangeMultiplier.multiply(x[j].divide((x[j].subtract(x[i]))));
+			}
+		}
+		System.out.println("LagrangeMultiplier for i = "+i+" is "+LagrangeMultiplier+".");
+		return LagrangeMultiplier;
+		
+		
+	}
+	
+	
+}
---
+++ Project Folder/initialize.java
@@ -0,0 +1,172 @@
+package edu.gatech.scs.pwd.hardening;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import edu.gatech.scs.pwd.hardening.encryptAndDecrypt;
+
+import java.math.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class initialize {
+
+
+	/* Random Prime Number */
+	BigInteger q;
+
+	/* Coefficients */
+	BigInteger[] c = new BigInteger[15];
+
+	
+	//a is the alpha value for account a's ith feature
+	BigInteger[] a=new BigInteger[15];
+
+	//b is the beta value for account a's ith feature
+	BigInteger[] b=new BigInteger[15];
+
+	
+	
+	/* Constructor. */
+	public initialize(){
+		//create a BigInteger object
+		BigInteger l;
+		
+		//Length of prime=160bits
+		int bitlength=160;
+		
+		//Create a random object
+		Random rnd= new Random();
+		
+		//assign probablePrime result to q using bit length and rnd.	
+		q=BigInteger.probablePrime(bitlength, rnd);
+		String str= "ProbablePrime of bitlength"+bitlength+" is " +q;
+		System.out.println(str);
+	}
+
+	
+	/* To choose a random polynomial f(x)=ckxk+⋯+c1x+c0 of degree k in the polynomial ring Zq[x] simply means choosing k+1 random coefficients c0,c1,⋯,ck uniformly from the finite field Zq. However, the additional constraint that f(0)=c0 should take a particular fixed value means that, in fact, only the k coefficients c1,…,ck can be chosen randomly, while the constant coefficient c0 is fixed.
+	http://crypto.stackexchange.com/questions/6455/how-to-generate-a-random-polynomial-of-degree-m	*/		
+			
+	public void Polynomials(BigInteger q){
+		// We need to generate 15 Random co-efficients and the c0 should be Hpwd.
+		Random rand= new Random();
+		for( int i=0;i <15;i++)
+		{
+			//Changing the value of c[0] to the Hpwd. Assuming Hpwd=12345
+			if(i==0){
+				
+				c[0]= BigInteger.valueOf(1234567890);
+			}
+				
+			else	{
+			c[i]=BigInteger.valueOf(rand.nextInt(1000)+1);  //Changed this from q to 100: Do Check again(Consult TA)
+			System.out.println("Coefficient of x^"+i+" is "+c[i]);
+			}
+		}
+		
+	}
+
+
+	/* Function to compute the value of alpha and beta */
+	public void Instruction_Table()
+	{
+		//z[i] stands for the ith value of y for an account 'a'--> Similar to y(a,i,1) 
+		BigInteger[] z=new BigInteger[15];
+	
+		//y[i] stands for the ith value of y for an account 'a'--> Similar to y(a,i,0) 
+		BigInteger[] y=new BigInteger[15];
+	
+
+		/* Using Formulas for alpha and beta
+			a[i]=y[i] + G(r,pwd)(2i)mod q
+			b[i]=z[i] + G(r,pwd)(2i)mod q */
+		
+		for(int i=0;i<15;i++)
+		{
+			a[i]= (Polynomial_Calculation(2*i).add(generateHMac("password", Integer.toString(2*i), "HmacSHA256"))).mod(q);
+			System.out.println(a[i]);
+			b[i]= (Polynomial_Calculation((2*i)+1).add(generateHMac("password", Integer.toString((2*i)+1), "HmacSHA256"))).mod(q);
+			System.out.println(b[i]);
+		}
+
+	}
+
+	
+	
+	public long bytesToLong(byte[] bytes) 
+	{
+	    
+        long r = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            r = r << 8;
+            r += bytes[i];
+        }
+	//System.out.println(r);
+        return r;
+    }
+
+	
+	
+	/* Calculating value of Polynomial required for alpha and beta. */
+	
+	public BigInteger Polynomial_Calculation(int x)
+	{
+		BigInteger f=BigInteger.valueOf(0);
+		for (int i=0;i<15;i++)
+		{
+			BigInteger mul = new BigDecimal(Math.pow(x,i)).toBigInteger();
+			f=f.add(c[i].multiply(mul));
+		}
+		//System.out.println(f);
+		return f;
+	}
+	
+	
+	/* Function to Compute Keyed Hash */
+	public BigInteger generateHMac(String secretKey, String data, String algorithm /* e.g. "HmacSHA256" */)
+	{
+
+	    	SecretKeySpec signingKey = new SecretKeySpec(secretKey.getBytes(), algorithm);
+
+		try {
+			Mac mac = Mac.getInstance(algorithm);
+			mac.init(signingKey);
+			//System.out.println(mac.doFinal(data.getBytes()));
+			
+			return new BigInteger(mac.doFinal(data.getBytes()));
+		    }
+		    catch(InvalidKeyException e) {
+			throw new IllegalArgumentException("invalid secret key provided (key not printed for security reasons!)");
+		    }
+		    catch(NoSuchAlgorithmException e) {
+			throw new IllegalStateException("the system doesn't support algorithm " + algorithm, e);
+		    }
+	}
+
+	
+	/* History File Creation and Updation. */
+	public void History_File()
+	{
+		try
+		{
+			
+			File f = new File("History_File.txt");
+			FileOutputStream History_file= new FileOutputStream("History_File.txt");
+			//long size = f.getTotalSpace();
+			//System.out.println("Size of file: " + size);
+			History_file.write("0,0,0,0,0,0,0,0,0,0,0,0,0,0,0".getBytes());
+			
+			History_file.close();
+		}
+		catch(Exception e){
+			System.err.println(e);}
+		
+	}
+	
+	
+}
